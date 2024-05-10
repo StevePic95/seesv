30b3abc28901010f41e61e84e33535deb336c057
Stephen <piccol24@students.rowan.edu>
2024-05-10 10:06:52 -0400

    base program; just takes a file as input and prints it to console

---
+++ seesv/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.CommandLine;
+
+namespace seesv;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var fileOption = new Option<FileInfo?>(
+            name: "--file",
+            description: "The csv file to use as input.");
+
+        var rootCommand = new RootCommand("A simple CLI for viewing and modifying CSV files.");
+        rootCommand.AddOption(fileOption);
+        
+        rootCommand.SetHandler((file) =>
+            {
+                ReadFile(file!);
+            },
+            fileOption);
+
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    static void ReadFile(FileInfo file)
+    {
+        File.ReadLines(file.FullName).ToList()
+            .ForEach(line => Console.WriteLine(line));
+    }
+}
